8caf8b9558e480a65e8e5be540a1e1721230a3f1
ASHUTOSH NAIK <108534953+Ashutosh-360@users.noreply.github.com>
2022-09-01 15:56:00 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
 // declare class
+class Shape 
+{
+  constructor() 
+  {
+    this.color = "blue";
+  }
+  drawShape() 
+  {}
+  calculateArea() 
+  {}
+}
+
+module.export = Shape;
 
 // export class using module.exports
